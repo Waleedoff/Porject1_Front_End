7ecf786a4d24a0a97bd9bdecf2e13a64601deb2e
Waleedoff <77509215+Waleedoff@users.noreply.github.com>
2022-06-25 22:12:49 +0300

    last version

--- project1_F-E/js/main.js
+++ project1_F-E/js/main.js
@@ -1,19 +1,33 @@
-let loginPage = "login/login.html"
-document.getElementById("gosome").onclick =  () => {
-    alert('You Need To Login First')
-    window.location = loginPage;
+let loginPage = "login/login.html" //path to login page
+
+iphone11 = document.getElementById('iphone11'); // iphone11 link in the navbar (dropdown)
+iphone13 = document.getElementById('iphone13');
+
+iphone13.onclick = () => { //when you click on the first card
+    alert('You Need To Login First') //show message says --> Login First
+    window.location = loginPage; // to the window at ==> LoginPage.
+
+}
+iphone11.onclick = () => { //when you click on the first card
+    alert('You Need To Login First') //show message says --> Login First
+    window.location = loginPage; // to the window at ==> LoginPage.
 
 }
 
 
-// iphoneColors js
 
-// let img = document.querySelector('.img');
 
-// let iphoneColors = function (phone){
-//     img.src = phone
-// }
-// alert('hi')
-
-// let username = document.querySelector('.input-box').value="username";
-// console.log(username)
+// iphone colors and container background. 
+let img = document.querySelector('.img')
+let Container = document.querySelector('.container')
+let iphoneColors = function (phone) {
+    img.src = phone
+    window.localStorage.setItem("img", phone)
+    window.localStorage.getItem(img.src);
+}
+window.localStorage.getItem(img.src);
+// document.body.style.backgroundColor =   window.localStorage.getItem(img.src);
+function colors(color) {
+    // Container.style.backgroundColor = color;
+    Container.style.backgroundColor = color
+}
